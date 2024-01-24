713fd7ba4ef74ea0d0388f8e2529f20e74156edd
Heowonhyeak <rkdcjf014777@gmail.com>
2024-01-24 17:42:52 +0900

    강사 방법 주석처리 (폴더가 없어 파일이 난잡)

--- 26.With.py
+++ 26.With.py
@@ -38,9 +38,9 @@ for weekly in range(1, 51):
         weekly_file.write("업무 요약 : \n")
 
 ## 강사 방법
-for i in range(1, 51):
-    with open(str(i)+"주차.txt", "w", encoding="utf8") as report_file:
-        report_file.write("- {0} 주차 주간 보고 -\n".format(i))
-        report_file.write("부서 : \n")
-        report_file.write("이름 : \n")
-        report_file.write("업무 요약 : \n")
+# for i in range(1, 51):
+#     with open(str(i)+"주차.txt", "w", encoding="utf8") as report_file:
+#         report_file.write("- {0} 주차 주간 보고 -\n".format(i))
+#         report_file.write("부서 : \n")
+#         report_file.write("이름 : \n")
+#         report_file.write("업무 요약 : \n")
